e89dacd869746c4a3880321708c7104612aaf3fc
swayamsharma19 <swayamsharma1599@gmail.com>
2026-03-18 11:34:15 +0530

    Aaj ki progress

---
+++ bestTimetoBuyStock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxProfit=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int profit=prices[j]-prices[i];
+                maxProfit=max(maxProfit,profit);
+            }
+        }
+        return maxProfit;
+    }
+};
