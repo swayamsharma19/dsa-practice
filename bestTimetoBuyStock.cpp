04ca4d56956ac8f8edd0cd2f9940997f66e4f953
swayamsharma19 <swayamsharma1599@gmail.com>
2026-03-18 11:38:36 +0530

    Change in the code

--- bestTimetoBuyStock.cpp
+++ bestTimetoBuyStock.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int maxProfit=0;
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int profit=prices[j]-prices[i];
-                maxProfit=max(maxProfit,profit);
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];  
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit = max(maxProfit, profit);
             }
         }
+
         return maxProfit;
     }
 };
