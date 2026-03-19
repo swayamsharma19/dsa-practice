1e371c128d50cb814a56aaa93e3b76b08d0dac0f
swayamsharma19 <swayamsharma1599@gmail.com>
2026-03-19 11:53:30 +0530

    modified

--- KthMissingpostive.int.cpp
+++ KthMissingpostive.int.cpp
@@ -1,5 +1,7 @@
 // In a perfect sequence without missing numbers the i'th number should be i+1, if any number is missing then arr[i] becomes greater then i+1 and the number of missing elements before arr[i] is arr[i]-(i+1)
 
+// At each index we check if the number of missing elements so far is at least k , the first index where arr[i] is greater than k+i indicates that the k'th missing number is before that index and is equal to k+i, if no such index is found the k'th missing number lies beyond the last element and is k+n 
+
 #include<iostream>
 #include<vector>
 using namespace std;
