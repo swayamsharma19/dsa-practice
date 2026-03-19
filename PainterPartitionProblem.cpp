824879840c67657511cc1bbef84359cdc677a6f7
swayamsharma19 <swayamsharma1599@gmail.com>
2026-03-19 11:03:10 +0530

    PainterPartitionProblem

---
+++ PainterPartitionProblem.cpp
@@ -0,0 +1,63 @@
+// SAME AS ALLOCATE MIN PAGES 
+
+#include<iostream>
+#include<vector>
+#include <algorithm> // for *maxElement
+#include <numeric>  // for accumulate 
+using namespace std;
+
+class Solution
+{
+public:
+    int noPages(vector<int> &arr, int pages)
+    {
+        int n = arr.size();
+        int sum = 0;   // 👉 ye current student ke total pages track karta hai
+        int student = 1;  
+
+        for (int i = 0; i < n; i++)
+        {
+            if (sum + arr[i] > pages)
+            {
+                student++;
+                sum = arr[i];
+            }
+            else
+            {
+                sum += arr[i];
+            }
+        }
+
+        return student;
+    }
+    
+    int minTime(vector<int> &arr, int k)
+    {
+        if (k > arr.size())
+            return -1;
+
+        int result = -1;
+
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            int pages = noPages(arr, mid);
+
+            if (pages <= k)
+            {
+                result = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};
