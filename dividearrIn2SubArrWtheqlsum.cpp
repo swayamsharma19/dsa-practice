6c7f085c06c9ec13dc2423588a496b6986e9f698
swayamsharma19 <swayamsharma1599@gmail.com>
2026-03-24 19:43:52 +0530

    Divide array in 2 SubArray with equal Sum

--- dividearrIn2SubArrWtheqlsum.cpp
+++ dividearrIn2SubArrWtheqlsum.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int arr[]={3,4,-2,5,8,20,-10,8};
-    int n=8;
+int main()
+{
+    int arr[] = {3, 4, -2, 5, 8, 20, -10, 8};
+    int n = 8;
 
-    int ans=0;
-    int totalSum=0;
+    int ans = 0;
+    int totalSum = 0;
 
-    for(int i=0;i<n;i++){
-        totalSum+=arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += arr[i];
     }
 
-    int prefix=0;
+    int prefix = 0;
 
-    for(int i=0;i<n-1;i++){
-        prefix+=arr[i];
-        ans=totalSum-prefix;
+    for (int i = 0; i < n - 1; i++)
+    {
+        prefix += arr[i];
+        ans = totalSum - prefix;
 
-        if(ans==prefix){
-            cout<<"it is possible"<<endl;
+        if (ans == prefix)
+        {
+            cout << "it is possible" << endl;
             return 1;
         }
     }
 
-    cout<<"not possible"<<endl;
+    cout << "not possible" << endl;
     return 0;
 
     return 0;
