27c122dafdccfb7ff684c67b59c5bd9c9855997b
swayamsharma19 <swayamsharma1599@gmail.com>
2026-04-07 11:01:07 +0530

    h

--- palindromeLinkedList.cpp
+++ palindromeLinkedList.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-
     // 🔧 bas ye function add kiya hai
-    ListNode* reverseLL(ListNode* head){
-        ListNode* prev = NULL;
-        ListNode* curr = head;
+    ListNode *reverseLL(ListNode *head)
+    {
+        ListNode *prev = NULL;
+        ListNode *curr = head;
 
-        while(curr != NULL){
-            ListNode* nextNode = curr->next;
+        while (curr != NULL)
+        {
+            ListNode *nextNode = curr->next;
             curr->next = prev;
             prev = curr;
             curr = nextNode;
@@ -18,25 +20,32 @@ public:
         return prev;
     }
 
-    bool isPalindrome(ListNode* head) {
-        if(head==nullptr || head->next==nullptr){
+    bool isPalindrome(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
             return true;
         }
-        ListNode* slow=head; ListNode* fast=head;
-        while(fast->next!=NULL && fast ->next->next !=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* newHead=reverseLL(slow->next);
-        ListNode* first=head; ListNode* second=newHead;
+        ListNode *newHead = reverseLL(slow->next);
+        ListNode *first = head;
+        ListNode *second = newHead;
 
-        while(second!=nullptr){
-            if(first->val!=second->val){
+        while (second != nullptr)
+        {
+            if (first->val != second->val)
+            {
                 reverseLL(newHead);
                 return false;
             }
-            first=first->next;
-            second=second->next;
+            first = first->next;
+            second = second->next;
         }
         reverseLL(newHead);
         return true;
