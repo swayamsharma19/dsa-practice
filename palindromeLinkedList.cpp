e1874ec70c1f1d6ccbcee9f742a86591ef48fa77
swayamsharma19 <swayamsharma1599@gmail.com>
2026-04-07 10:48:18 +0530

    palindromeLinkedList

---
+++ palindromeLinkedList.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+
+    // 🔧 bas ye function add kiya hai
+    ListNode* reverseLL(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){
+            return true;
+        }
+        ListNode* slow=head; ListNode* fast=head;
+        while(fast->next!=NULL && fast ->next->next !=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newHead=reverseLL(slow->next);
+        ListNode* first=head; ListNode* second=newHead;
+
+        while(second!=nullptr){
+            if(first->val!=second->val){
+                reverseLL(newHead);
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        reverseLL(newHead);
+        return true;
+    }
+};
