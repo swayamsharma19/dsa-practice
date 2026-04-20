631c716d44e520e33ccc977590e0660b17a0ba1b
swayamsharma19 <swayamsharma1599@gmail.com>
2026-04-20 12:21:00 +0530

    Implement queue using stack

---
+++ queueusingstack.cpp
@@ -0,0 +1,49 @@
+class MyQueue
+{
+public:
+    stack<int> inStack;
+    stack<int> outStack;
+
+    MyQueue()
+    {
+    }
+
+    void push(int x)
+    {
+        inStack.push(x);
+    }
+
+    int pop()
+    {
+        if (outStack.empty())
+        {
+
+            while (!inStack.empty())
+            {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        int val = outStack.top();
+        outStack.pop();
+        return val;
+    }
+
+    int peek()
+    {
+        if (outStack.empty())
+        {
+            while (!inStack.empty())
+            {
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        return outStack.top();
+    }
+
+    bool empty()
+    {
+        return inStack.empty() && outStack.empty();
+    }
+};
